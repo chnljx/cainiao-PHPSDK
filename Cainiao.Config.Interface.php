24090053e2bb42576f1da97489ad316c2d54c13d
Michael Lee <chnljx@126.com>
2020-05-09 19:18:06 +0800

    init

--- Cainiao.Config.Interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class CainiaoConfigInterface
-{
-    public abstract function GetAppSecret();
-
-    public abstract function GetLogisticProviderId();
-
-    public abstract function GetProductType();
-}
--- Cainiao.Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-class CainiaoException extends Exception
-{
-    public function errorMessage()
-    {
-        return $this->getMessage();
-    }
-}
